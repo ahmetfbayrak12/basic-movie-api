74cd1261e92fbb780714c12ddfbce2ba7d7f783e
ahmetfbayrak <ahmetfbayrak12@gmail.com>
2023-05-14 23:47:21 +0300

    basic movie api backend initial implementation

---
+++ src/main/java/dev/afb/movies/MovieService.java
@@ -0,0 +1,21 @@
+package dev.afb.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private IMovieRepository movieRepository;
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieByImdbId(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
